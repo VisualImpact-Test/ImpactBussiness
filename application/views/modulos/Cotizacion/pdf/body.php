72f81b45bbbf30618557a057556ec1ea6a54c35d
AaronDevl <aaron.ccenta@gmail.com>
2022-08-31 11:07:18 -0500

    anexos

--- application/views/modulos/Cotizacion/pdf/body.php
+++ application/views/modulos/Cotizacion/pdf/body.php
@@ -17,8 +17,8 @@
             <td style="width: 100px;"></td>
         </tr>
         <tr>
-            <td colspan= "2" style="text-align: left; height: 20px;">CENTRO DE COSTO:</td>
-            <td colspan= "4" style="text-align: justify; height: 20px;"><?= $cabecera['cuentaCentroCosto'] ?></td>
+            <td colspan="2" style="text-align: left; height: 20px;">CENTRO DE COSTO:</td>
+            <td colspan="4" style="text-align: justify; height: 20px;"><?= $cabecera['cuentaCentroCosto'] ?></td>
         </tr>
     </table>
 </div><br>
@@ -39,17 +39,17 @@
             <tr>
                 <td style="text-align: center;"><?= $key + 1 ?></td>
                 <td style="text-align: left;">
-                <?= verificarEmpty($row['item'], 3) ?>
-                    <?if(!empty($row['caracteristicas'])){?>
+                    <?= verificarEmpty($row['item'], 3) ?>
+                    <? if (!empty($row['caracteristicas'])) { ?>
                         <p>
-                            <?=$row['caracteristicas']?>
+                            <?= $row['caracteristicas'] ?>
                         </p>
-                    <?}?>
+                    <? } ?>
                 </td>
                 <td style="text-align: right;"><?= verificarEmpty($row['cantidad'], 3) ?></td>
                 <!-- <td style="text-align: right;"><?= empty($row['costo']) ? "-" : moneda($row['costo']); ?></td> -->
                 <!-- <td style="text-align: left;"><?= verificarEmpty($row['gap'], 2) . '%' ?></td> -->
-                <td style="text-align: right;"><?= empty($row['precio']) ? moneda(verificarEmpty($row['costo'],2)) : moneda($row['precio']); ?></td>
+                <td style="text-align: right;"><?= empty($row['precio']) ? moneda(verificarEmpty($row['costo'], 2)) : moneda($row['precio']); ?></td>
                 <td style="text-align: right;"><?= empty($row['subtotal']) ? "-" : moneda($row['subtotal']); ?></td>
 
             </tr>
@@ -60,20 +60,30 @@
         <tr class="height:100px">
             <td colspan="4" class="text-right">
                 <p>SUB TOTAL</p>
-                <p>FEE <?=!empty($cabecera['fee']) ? $cabecera['fee'].'%' : '' ?></p>
-                <?if(!empty($cabecera['igv'])){?>
-                <p>IGV</p>
-                <?}?>
+                <p>FEE <?= !empty($cabecera['fee']) ? $cabecera['fee'] . '%' : '' ?></p>
+                <? if (!empty($cabecera['igv'])) { ?>
+                    <p>IGV</p>
+                <? } ?>
                 <p>TOTAL GENERAL <?= empty($cabecera['igv']) ? '(No incluye igv)' : '' ?></p>
             </td>
             <td class="text-right">
                 <p><?= moneda($cabecera['total']) ?></p>
-                <p><?= moneda(($cabecera['fee_prc']) ) ?></p>
-                <?if(!empty($cabecera['igv'])){?>
-                    <p><?=moneda($cabecera['igv_prc'] * IGV)?></p>
-                <?}?>
+                <p><?= moneda(($cabecera['fee_prc'])) ?></p>
+                <? if (!empty($cabecera['igv'])) { ?>
+                    <p><?= moneda($cabecera['igv_prc'] * IGV) ?></p>
+                <? } ?>
                 <p><?= moneda($cabecera['total_fee_igv'])  ?></p>
             </td>
         </tr>
     </tfoot>
 </table>
+
+<? if (!empty($anexos)) { ?>
+    <h3>Anexos</h3>
+    <? foreach ($anexos as $anexo) { ?>
+        <div class="ui fluid image content-lsck-capturas" data-id="<?= $anexo['idCotizacionDetalleArchivo'] ?>">
+            <img height="100" src="<?= RUTA_WASABI . "cotizacion/{$anexo['nombre_archivo']}" ?>" class="img-lsck-capturas img-responsive img-thumbnail">
+        </div>
+    <? } ?>
+
+<? } ?>
