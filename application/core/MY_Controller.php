610f3a5628a5d5007ceef2e587519b698b7bd7af
Eder Alata <eder.alata@visualimpact.com.pe>
2023-12-15 13:20:52 -0500

    version js

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -47,7 +47,7 @@ class MY_Controller extends CI_Controller
 	public function __construct()
 	{
 		parent::__construct();
-		$this->version = '1.1.57';
+		$this->version = '1.1.58';
 		date_default_timezone_set("America/Lima");
 
 		$_SESSION['idCuenta'] = $this->session->userdata('idCuenta');
