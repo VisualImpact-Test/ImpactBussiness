265cfe55229a9c12d4fa0c0c5c068bb15464d467
BILL SALAZAR <billsalazar14@gmail.com>
2024-01-15 09:40:22 -0500

    20240115.1
    Signed-off-by: BILL SALAZAR <billsalazar14@gmail.com>

--- application/controllers/Finanzas/ProveedorServicio.php
+++ application/controllers/Finanzas/ProveedorServicio.php
@@ -41,38 +41,13 @@ class ProveedorServicio extends MY_Controller
 	{
 		$result = $this->result;
         $dataParaVista = [];
-		$data = $this->model->obtenerProveedorServicio()['query']->result_array();
+		$dataParaVista['proveedorServicio'] = $this->model->obtenerProveedorServicio()['query']->result_array();
 
-		foreach ($data as $key => $row) {
-			$dataParaVista[] = [
-				'idProveedorServicio' => $row['idProveedorServicio'],
-				'ruc' => $row['ruc'],
-				'razonSocial' => $row['razonSocial'],
-				'direccion' => $row['direccion'],
-				'nombreContacto' => $row['nombreContacto'],
-				'numeroContacto' => $row['numeroContacto'],
-				'correoContacto' => $row['correoContacto'],
-				'estado' => $row['estado'],
-				'idEstado' => $row['idProveedorEstado'],
-				'estadoIcono' => $row['estadoIcono'],
-				'estadoToggle' => $row['estadoToggle'],
-				'departamento' => $row['departamento'],
-				'provincia' => $row['provincia'],
-				'distrito' => $row['distrito'],
-				'monto' => $row['monto'],
-				'diaPago' => $row['diaPago'],
-				'frecuenciaPago' => $row['frecuenciaPago'],
-				'fechaInicio' => $row['fechaInicio'],
-				'fechaTermino' => $row['fechaTermino'],
-				'descripcionServicio' => $row['descripcionServicio'],
-				'simbolo' => $row['simbolo'],
-				'distrito' => $row['distrito'],
-			];
-		}
+		
 
 		$html = getMensajeGestion('noRegistros');
 		if (!empty($dataParaVista)) {
-			$html = $this->load->view("modulos/Finanzas/ProveedorServicio/reporte", ['datos' => $dataParaVista], true);
+			$html = $this->load->view("modulos/Finanzas/ProveedorServicio/reporte", $dataParaVista, true);
 		}
 
         $result['result'] = 1;
@@ -93,6 +68,75 @@ class ProveedorServicio extends MY_Controller
 
     }
 
+	public function formularioRegistroProveedorServicioPago()
+	{
+		$result = $this->result;
+
+		$dataParaVista = [];
+		$dataParaVista['proveedorServicio'] = $this->db->get_where('finanzas.proveedorServicio')->result_array();
+		$dataParaVista['moneda'] = $this->db->get_where('compras.moneda', array('estado' => '1'))->result_array();
+		$dataParaVista['frecuenciaPago'] = $this->db->get_where('finanzas.frecuenciaPagoProveedorServicioPago', array('estado' => '1'))->result_array();
+
+		$result['result'] = 1;
+		$result['msg']['title'] = 'Registrar Pago Proveedor Servicio';
+		$result['data']['html'] = $this->load->view("modulos/Finanzas/ProveedorServicio/formularioRegistro", $dataParaVista, true);
+
+		echo json_encode($result);
+	}
+
+	public function formularioActualizacionProveedorServicioPago()
+	{
+		$result = $this->result;
+		$post = json_decode($this->input->post('data'), true);
+
+		$dataParaVista = [];
+		$dataParaVista['proveedorServicioPago'] = $this->model->obtenerProveedorServicio(['idProveedorServicioPago' => $post['idProveedorServicioPago']])['query']->result_array();
+		
+		$result['result'] = 1;
+		$result['msg']['title'] = 'Actualizar Pago Proveedor Servicio';
+		$result['data']['html'] = $this->load->view("modulos/Finanzas/ProveedorServicio/formularioActualizacion", $dataParaVista, true);
+
+		echo json_encode($result);
+	}
+
+	public function registrarProveedorServicioPago()
+	{
+		$this->db->trans_start();
+		$result = $this->result;
+		$post = json_decode($this->input->post('data'), true);
+
+		$data = [];
+		$data['insert'] = [
+			'idProveedorServicio' => $post['proveedorServicio'],
+			'flagFijo' => isset($post["chkMontoFijo"]) ? 1 : 0,
+			'monto' => isset($post["chkMontoFijo"]) ? $post['monto'] : null,
+			'diaPago' => $post['diaPago'],
+			'frecuenciaPago' => $post['frecuenciaPago'],
+			'fechaInicio' => $post['deadlineInicio'],
+			'fechaTermino' => verificarEmpty($post['deadlineTermino'], 4),
+			'idMoneda' => $post['moneda'],
+			'descripcionServicio' => verificarEmpty($post['informacionAdicional'], 4)
+		];
+		
+		$data['tabla'] = 'finanzas.proveedorServicioPago';
+		$insert = $this->model->insertarProveedorServicioPago($data);
+
+		if (!$insert) {
+			$result['result'] = 0;
+			$result['msg']['title'] = 'Alerta!';
+			$result['msg']['content'] = getMensajeGestion('registroErroneo');
+			goto respuesta;
+		} else {
+			$result['result'] = 1;
+			$result['msg']['title'] = 'Hecho!';
+			$result['msg']['content'] = getMensajeGestion('registroExitoso');
+		}
+
+		$this->db->trans_complete();
+		respuesta:
+		echo json_encode($result);
+	}
+
 	public function actualizarEstadoProveedorServicio()
 	{
 		$result = $this->result;
@@ -104,7 +148,7 @@ class ProveedorServicio extends MY_Controller
 		$data['tabla'] = 'finanzas.proveedorServicio';
 		$data['where'] = ['idProveedorServicio' => $post['idProveedorServicio']];
 
-		$update = $this->model->actualizarProveedor($data);
+		$update = $this->model->actualizarProveedorServicioPago($data);
 		$data = [];
 
 		if (!$update['estado']) {
--- public/assets/custom/js/Finanzas/proveedorServicio.js
+++ public/assets/custom/js/Finanzas/proveedorServicio.js
@@ -16,11 +16,11 @@ var ProveedorServicio = {
 				, 'url': ProveedorServicio.url + ruta
 				, 'contentDetalle': ProveedorServicio.contentDetalle
 			};
-           // console.log(config);
+			// console.log(config);
 			Fn.loadReporte_new(config);
 			Fn.showLoading(false);
 		});
-	
+
 		$(document).on('click', '.btn-actualizar-estado', function () {
 			++modalId;
 
@@ -37,8 +37,99 @@ var ProveedorServicio = {
 			});
 		});
 
+		$(document).on("change", ".chkMontoFijo", function () {
+			let this_ = $(this);
+			let check = this_.is(':checked');
+			if (check) {
+				$('.fijo').removeClass('d-none');
+				$('.onlyNumbers').attr('patron', 'requerido')
+			} else {
+				$('.fijo').addClass('d-none');
+				$('.onlyNumbers').removeAttr('patron')
+			}
+
+		});
+
+		$(document).on('click', '#btn-registrarProveedorServicioPago', function () {
+			++modalId;
+
+			let jsonString = { 'data': '' };
+			let config = { 'url': ProveedorServicio.url + 'formularioRegistroProveedorServicioPago', 'data': jsonString };
+
+			$.when(Fn.ajax(config)).then((a) => {
+				let btn = [];
+				let fn = [];
+
+				fn[0] = 'Fn.showModal({ id:' + modalId + ',show:false });';
+				btn[0] = { title: 'Cerrar', fn: fn[0] };
+				fn[1] = 'Fn.showConfirm({ idForm: "formRegistroProveedorServicioPago", fn: "ProveedorServicio.registrarProveedorServicioPago()", content: "¿Esta seguro de registrar el pago?" });';
+				btn[1] = { title: 'Registrar', fn: fn[1] };
+
+				Fn.showModal({ id: modalId, show: true, title: a.msg.title, frm: a.data.html, btn: btn, width: '50%' });
+				Fn.loadSemanticFunctions();
+				Fn.loadDimmerHover();
+			});
+		});
+
+		$(document).on('click', '.btn-editar', function () {
+			++modalId;
+
+			let id = $(this).parents('tr:first').data('id');
+			let data = { 'idProveedorServicioPago': id };
+
+			let jsonString = { 'data': JSON.stringify(data) };
+			let config = { 'url': ProveedorServicio.url + 'formularioActualizacionProveedorServicioPago', 'data': jsonString };
+
+			$.when(Fn.ajax(config)).then((a) => {
+				let btn = [];
+				let fn = [];
+
+				fn[0] = 'Fn.showModal({ id:' + modalId + ',show:false });';
+				btn[0] = { title: 'Cerrar', fn: fn[0] };
+				fn[1] = 'Fn.showConfirm({ idForm: "formActualizacionProveedores", fn: "Proveedor.actualizarProveedor()", content: "¿Esta seguro de actualizar el proveedor?" });';
+				btn[1] = { title: 'Actualizar', fn: fn[1] };
+
+				Fn.showModal({ id: modalId, show: true, title: a.msg.title, frm: a.data.html, btn: btn, width: '50%' });
+				Fn.loadSemanticFunctions();
+				Fn.loadDimmerHover();
+				Proveedor.bancos = a.data.bancos;
+				Proveedor.tiposCuentaBanco = a.data.tiposCuentaBanco;
+				Proveedor.divInfoBancData = '<div class="row InfoBancData">' + $('#divInfoBancData').html() + '</div>';
+			});
+
+		});
+
+		$(document).on("keypress", "#diaPago", function (event) {
+			var value = $(this).val();
+			var key = event.which || event.keyCode;
+			var dia = value + String.fromCharCode(key);
+			if (isNaN(dia) === true || dia < 1 || dia > 31) {
+				return false;
+			}
+			return true;
+		});
+
 		HTCustom.load();
-	}
+	},
+
+	registrarProveedorServicioPago: function () {
+		let jsonString = { 'data': JSON.stringify(Fn.formSerializeObject('formRegistroProveedorServicioPago')) };
+		let url = ProveedorServicio.url + "registrarProveedorServicioPago";
+		let config = { url: url, data: jsonString };
+
+		$.when(Fn.ajax(config)).then(function (b) {
+			++modalId;
+			var btn = [];
+			let fn = 'Fn.showModal({ id:' + modalId + ',show:false });';
+
+			if (b.result == 1) {
+				fn = 'Fn.closeModals(' + modalId + ');$("#btn-filtrarProveedorServicio").click();';
+			}
+
+			btn[0] = { title: 'Continuar', fn: fn };
+			Fn.showModal({ id: modalId, show: true, title: b.msg.title, content: b.msg.content, btn: btn, width: '40%' });
+		});
+	},
 }
 
 ProveedorServicio.load();
