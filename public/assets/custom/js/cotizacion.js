f343e6bf96e80420cc2f136cbdd04ef16839c7ed
Christopher Iparraguirre <christopher.iparraguirre@visualimpact.com.pe>
2024-02-02 10:27:54 -0500

    v3

--- public/assets/custom/js/cotizacion.js
+++ public/assets/custom/js/cotizacion.js
@@ -1242,10 +1242,12 @@ var Cotizacion = {
 		$.when(Fn.ajax(config)).then(function (b) {
 			++modalId;
 			var btn = [];
-			let fn = 'Fn.showModal({ id:' + modalId + ',show:false });';
+			let fn = 'Fn.closeModals(10);$("#btn-filtrarCotizacion").click();';
 
 		
-
+			// if (b.result == 1) {
+			// 	fn[0] = 'Fn.closeModals(10);$("#btn-filtrarCotizacion").click();';
+			// }
 			btn[0] = { title: 'Continuar', fn: fn };
 			Fn.showModal({ id: modalId, show: true, title: b.msg.title, content: b.msg.content, btn: btn, width: '40%' });
 
