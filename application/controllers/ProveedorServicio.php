745ec8e6252a9c92e59a04e1ae8d03a495fff8df
Eder Alata <eder.alata@visualimpact.com.pe>
2024-03-12 12:37:33 -0500

    ..

--- application/controllers/ProveedorServicio.php
+++ application/controllers/ProveedorServicio.php
@@ -99,7 +99,7 @@ class ProveedorServicio extends MY_Controller
 				foreach ($listDetalleCotProv as $vt) {
 					$it = $this->db->get_where('orden.ordenCompraDetalle', ['idOrdenCompra' => $vt['idOrdenCompra'], 'estado' => 1])->row_array()['idTipo'];
 
-					if ($it == COD_SERVICIO['id'] || $it == COD_TEXTILES['id']) {
+					if ($it == COD_SERVICIO['id'] /* || $it == COD_TEXTILES['id'] */) { // Anghy dice que TEXTILES YA NO REQUIERE VALIDACION DE ARTE 2024-03-12
 						$data[$k]['requiereValidacion'] = '1';
 						$data[$k]['mostrarValidacion'] = '1';
 						$data[$k]['solicitarFecha'] = '0';
