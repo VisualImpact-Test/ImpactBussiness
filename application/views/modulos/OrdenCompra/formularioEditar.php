17098dd3dc42b8a5585791c096db62ae644a2abe
Christopher Iparraguirre <christopher.iparraguirre@visualimpact.com.pe>
2024-03-12 10:07:56 -0500

    chris

--- application/views/modulos/OrdenCompra/formularioEditar.php
+++ application/views/modulos/OrdenCompra/formularioEditar.php
@@ -161,8 +161,7 @@
 				<div class="form-row">
 					<div class="form-group col-md-10">
 						<label class="font-weight-bold mb-0">Observación:</label>
-						<input class="form-control" name="observacion" value="<?= isset($oc[0]['observacion']) ? $oc[0]['observacion'] : '' ?>">
-					</div>
+						<input class="form-control" name="observacion" value="<?= isset($oc[0]['observacion']) ? $oc[0]['observacion'] : 'En caso de incumplimiento en fecha de entrega, se estará ejecutando penalidad del 1% por cada día de retraso.' ?>">					</div>
 					<div class="form-group col-md-2">
 						<label class="font-weight-bold mb-0">Mostrar Observación</label>
 						<div class="custom-control custom-switch custom-switch-lg">
